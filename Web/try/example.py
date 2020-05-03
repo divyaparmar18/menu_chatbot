bdea946f9899cc29101fe87b8648e7dbfacf6b1f
ITRS-333 <itrs-333@ITRS-333s-MacBook-Air.local>
2020-05-03 11:40:14 +0530

    removing extra files

--- Web/try/example.py
+++
@@ -1,6 +0,0 @@
-decade = {"1950": [{"position": 9,"movie_name": "Pather Panchali",},{"position": 3,"movie_name": "Aparajito",}],"1960": [{"position": 19,"movie_name": "Guide"},{"position": 33,"movie_name": "Mughal-E-Azam",}]}
-for i in decade.keys():
-	print i
-	decade[i].sort(key=lambda i: i["position"])
-print decade
-		
